631fc6fc488068d55add1d3b23f26cdcd5d6c9c1
Mariana Schmal <marianaschmal@mbp-von-mariana.speedport.ip>
2024-07-25 12:56:36 +0200

    added overview and landing page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { OverviewComponent } from './overview/overview.component';
 import { HeaderComponent } from './header/header.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     OverviewComponent,
     HeaderComponent,
+    LandingPageComponent,
    
   ],
   imports: [
